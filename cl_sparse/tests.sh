27df7caee4640cd215226b668ce5417d0f0789d1
Jan Pokorný <xpokor04@stud.fit.vutbr.cz>
2011-03-22 15:04:57 +0100

    tests.sh: watched changes in test runs

--- cl_sparse/tests.sh
+++ cl_sparse/tests.sh
@@ -2,9 +2,11 @@
 
 TESTSUITE_DIR=tests
 TESTSUITE_OUTPUTS=.dots
+TESTSUITE_LOGS=.logs
 TESTSUITE_IGNORE=.testignore
 
 ISOMORPHISM_TESTER="$PWD"/isomorphic
+DIFF=colordiff
 
 CL_SPARSE="$PWD"/test
 CL_GCC=gcc
@@ -34,9 +36,10 @@ function ok() {
 
 function bad() {
     let CNT_BAD+=1
+    echo $1 >> $3
     printf "%-61s" "$(basename $1)"
     echo -e "\033[31mBAD\033[0m"
-    if [ $# -gt 2 ]; then
+    if [ $# -gt 3 ]; then
         echo $2
     elif [ $GENERATE_BAD -ne 0 ]; then
         dot -Tpdf $1 > $1.pdf
@@ -70,8 +73,14 @@ function do_tests() {
     prelude
     clean >/dev/null
 
+    ###
     pushd $TESTSUITE_DIR >/dev/null
-    mkdir -p $TESTSUITE_OUTPUTS/sparse $TESTSUITE_OUTPUTS/gcc
+
+    mkdir -p $TESTSUITE_OUTPUTS/sparse $TESTSUITE_OUTPUTS/gcc $TESTSUITE_LOGS
+    PREV_SUMMARY_LOG=$TESTSUITE_LOGS/$(ls -1 -r $TESTSUITE_LOGS | head -n1)
+    SUMMARY_LOG=$TESTSUITE_LOGS/$(date +%y%m%d_%H%M%S).log.unsorted
+    echo $PREV_SUMMARY_LOG
+
     for SRC in $(find . -name "*.c" | sed "s/\.\//|/1" | cut -d"|" -f2); do
         echo $DLINE
         proceed_sparse $SRC $SRC.flow.dot $SRC.type.dot 2>&1 >/dev/null
@@ -80,7 +89,7 @@ function do_tests() {
         proceed_gcc    $SRC $SRC.flow.dot $SRC.type.dot 2>&1 >/dev/null
             tar cf - $SRC*.dot | ( cd $TESTSUITE_OUTPUTS/gcc; tar xfp -)
             rm -f -- $SRC*.dot
-        GCC_DOTS=$(find $TESTSUITE_OUTPUTS/gcc/. -path "*$SRC*.dot")       
+        GCC_DOTS=$(find $TESTSUITE_OUTPUTS/gcc/. -path "*$SRC*.dot")
         printf "%-61s %2i\n" $(echo -n $SRC | tr [:lower:] [:upper:]) \
                              $(echo $GCC_DOTS | wc -w)
         #echo $SLINE
@@ -97,18 +106,30 @@ function do_tests() {
             DOT_SPARSE=$(echo $DOT | sed s/gcc/sparse/1)
             $ISOMORPHISM_TESTER $DOT $DOT_SPARSE >/dev/null
             case $? in 0) ok  $DOT $DOT_SPARSE;;
-                       1) bad $DOT $DOT_SPARSE;;
-                       *) bad $DOT $DOT_SPARSE "unexpected retval";;
+                       1) bad $DOT $DOT_SPARSE $SUMMARY_LOG;;
+                       *) bad $DOT $DOT_SPARSE $SUMMARY_LOG "unexpected retval";;
             esac
             [ $KEEP_DOT_FILES -eq 0 ] && rm -f -- $DOT $DOT_SPARSE
         done
     done
-    popd >/dev/null
 
     echo $DLINE
+
     printf "Good %59d\n" $CNT_OK
     printf "Bad  %59d\n" $CNT_BAD
     printf "SUM  %59d\n" $(($CNT_OK+$CNT_BAD))
+
+    echo $DLINE
+
+    NEW_SUMMARY_LOG=$(echo $SUMMARY_LOG | sed "s/\.unsorted//1")
+    sort $SUMMARY_LOG -o $NEW_SUMMARY_LOG
+    rm -f $SUMMARY_LOG
+    $DIFF $NEW_SUMMARY_LOG $PREV_SUMMARY_LOG
+
+    [ $? -ne 0 ] && echo $DLINE
+
+    popd >/dev/null
+    ###
 }
 
 
