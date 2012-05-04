919771d821971edc8d554dc518d17b967fd69ee1
Jan Pokorný <pokorny_jan@seznam.cz>
2012-05-04 11:19:46 +0200

    tests.sh: fix thinko with batch mode
    Signed-off-by: Jan Pokorný <pokorny_jan@seznam.cz>

--- clsp/tests/tests.sh
+++ clsp/tests/tests.sh
@@ -86,7 +86,7 @@ run_clsp_i () {
     if [ "${BATCH}" -eq 0 ]; then
         ${CLSP_RUNNER} $1 -i -- "$2"
     else
-        yes 'n' | ${CLSP_RUNNER} $1 -i -- "$2"
+        ${CLSP_RUNNER} $1 -- "$2"
     fi
 }
 
